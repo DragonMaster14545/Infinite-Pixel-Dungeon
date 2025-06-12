1746bc26d2e9ddc63e6ddc25436f6e48f2198e70
jjbali <johnnollas5@gmail.com>
2025-06-12 15:14:45 +0800

    Added Raritize spell and its recipe

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/spells/Raritize.java
@@ -0,0 +1,112 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * Experienced Pixel Dungeon
+ * Copyright (C) 2019-2024 Trashbox Bobylev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.items.spells;
+
+import com.shatteredpixel.shatteredpixeldungeon.Challenges;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Transmuting;
+import com.shatteredpixel.shatteredpixeldungeon.items.Generator;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.armor.Armor;
+import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.Artifact;
+import com.shatteredpixel.shatteredpixeldungeon.items.fishingrods.FishingRod;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.Potion;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.brews.Brew;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.elixirs.Elixir;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.exotic.ExoticPotion;
+import com.shatteredpixel.shatteredpixeldungeon.items.rings.Ring;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.Scroll;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfTransmutation;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfUpgrade;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.exotic.ExoticScroll;
+import com.shatteredpixel.shatteredpixeldungeon.items.stones.Runestone;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.darts.TippedDart;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.plants.Plant;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.watabou.utils.Reflection;
+
+public class Raritize extends InventorySpell {
+	
+	{
+		image = ItemSpriteSheet.RECYCLE;
+
+		talentFactor = 2;
+		talentChance = 1/(float) Recipe.OUT_QUANTITY;
+	}
+
+	private static ItemSprite.Glowing COLOR = new ItemSprite.Glowing( 0x00FF00 );
+
+	@Override
+	public ItemSprite.Glowing glowing() {
+		return COLOR;
+	}
+
+	@Override
+	protected boolean usableOnItem(Item item) {
+		return (item instanceof Weapon || item instanceof Armor || item instanceof Ring || item instanceof Artifact
+				|| item instanceof Wand || item instanceof FishingRod);
+	}
+
+	@Override
+	protected void onItemSelected(Item item) {
+
+		item.randomizeRarity();
+
+		GLog.p(Messages.get(this, "raritized"));
+		curUser.sprite.emitter().start(Speck.factory(Speck.UP), 0.2f, 10);
+	}
+	
+	@Override
+	public long value() {
+		return (long)(60 * (quantity/(float) Recipe.OUT_QUANTITY));
+	}
+
+	@Override
+	public long energyVal() {
+		return (long)(12 * (quantity/(float) Recipe.OUT_QUANTITY));
+	}
+	
+	public static class Recipe extends com.shatteredpixel.shatteredpixeldungeon.items.Recipe.SimpleRecipe {
+
+		private static final int OUT_QUANTITY = 4;
+
+		{
+			inputs =  new Class[]{ScrollOfUpgrade.class, ScrollOfTransmutation.class};
+			inQuantity = new int[]{1, 1};
+			
+			cost = 75;
+			
+			output = Raritize.class;
+			outQuantity = OUT_QUANTITY;
+		}
+		
+	}
+}
