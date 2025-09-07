2823ffed3ba28b8355ada6c918e476847938b0fa
jjbali <114225456+jjbali@users.noreply.github.com>
2025-09-07 23:47:02 +0800

    fixed a crash on journal

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/MeleeWeapon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/MeleeWeapon.java
@@ -324,7 +324,7 @@ private static boolean evaluatingTwinUpgrades = false;
 
 			}
 		}
-		if (Dungeon.hero.buff(Kunai.WeaponCharge.class) != null){
+		if (Dungeon.hero.buff(Kunai.WeaponCharge.class) != null && Dungeon.hero != null){
 			return super.buffedLvl() + Dungeon.hero.buff(Kunai.WeaponCharge.class).stack;
 		}
 		return super.buffedLvl();
