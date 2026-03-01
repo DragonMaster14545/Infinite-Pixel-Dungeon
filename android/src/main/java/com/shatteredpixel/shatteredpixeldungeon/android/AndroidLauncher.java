f59e86571db965a80387a6569083590fdb09bf1c
jjbali <johnnollas5@gmail.com>
2026-03-01 14:43:54 +0800

    final v0.1.6c

--- android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
+++ android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
@@ -40,7 +40,7 @@ import com.badlogic.gdx.backends.android.AndroidAudio;
 import com.badlogic.gdx.backends.android.AsynchronousAndroidAudio;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeType;
 import com.badlogic.gdx.utils.GdxNativesLoader;
-import com.rohitss.uceh.UCEHandler;
+//import com.rohitss.uceh.UCEHandler;
 import com.shatteredpixel.shatteredpixeldungeon.SPDSettings;
 import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
 import com.shatteredpixel.shatteredpixeldungeon.services.news.News;
@@ -75,7 +75,7 @@ public class AndroidLauncher extends AndroidApplication {
             //let initialization continue for a moment so that we can set up things libGDX expects to be set up
         }
 
-        new UCEHandler.Builder(this).build();
+        //new UCEHandler.Builder(this).build();
 
         //there are some things we only need to set up on first launch
         if (instance == null) {
