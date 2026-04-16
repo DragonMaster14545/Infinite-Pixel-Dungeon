cf08dded2be111cc5349d2ffb4b055b1afe5df98
jjbali <johnnollas5@gmail.com>
2026-04-16 20:40:50 +0800

    final v0.1.9a

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/InfPDChanges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/InfPDChanges.java
@@ -72,6 +72,10 @@ public class InfPDChanges {
         changes.hardlight(0x00FFFF);
         changeInfos.add(changes);
 
+        changes = new ChangeInfo("A", false, "");
+        changes.hardlight(0x00FFFF);
+        changeInfos.add(changes);
+
         changes.addButton( new ChangeButton(Icons.STAIRS.get(), "New Rooms and Terrain Types",
                 "This update includes an _expansion to the dungeon's standard rooms!_ (from the ShPD, of course!)\n" +
                         "\n" +
