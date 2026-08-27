6fc896e702133594ef78f7ac1fde81c5c3b4a861
jjbali <johnnollas5@gmail.com>
2026-08-27 08:15:51 +0800

    added an ability to convert weapons into blueprints

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Recipe.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Recipe.java
@@ -194,7 +194,7 @@ public abstract class Recipe {
 	//*******
 
 	private static Recipe[] variableRecipes = new Recipe[]{
-
+		new WeaponBlueprint.Recipe()
 	};
 
 	private static Recipe[] oneIngredientRecipes = new Recipe[]{
@@ -309,8 +309,7 @@ public abstract class Recipe {
 	public static boolean usableInRecipe(Item item){
 		//only upgradeable thrown weapons and wands allowed among equipment items
 		if (item instanceof EquipableItem){
-			return item.isIdentified() && !item.cursed &&
-					item instanceof MissileWeapon && item.isUpgradable();
+			return item.isIdentified() && !item.cursed && item.isUpgradable();
 		} else if (item instanceof Wand) {
 			return item.isIdentified() && !item.cursed;
 		} else {
